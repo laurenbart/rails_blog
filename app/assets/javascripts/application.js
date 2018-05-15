263561c172de97ed5bb548bdb5d5958866cedb22
Lauren Bart <lmb54692@gmail.com>
2018-05-15 00:41:20 +0000

    jquery_api_ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,29 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
+
+
+
+const productSchoolLat = "37.796465"
+const productSchoolLng = "-122.402492"
+
+$.ajax({
+  url: '/api/v1/weather',
+  type: 'GET',
+  data: { lat: productSchoolLat, lng: productSchoolLng } ,
+  contentType: 'application/json; charset=utf-8',
+  success: function (response) {
+    console.log(response); 
+    var weatherContainer = $('<div>');
+    var tempContainer = $('<div>');
+    weatherContainer.html(response.currently.summary);
+    tempContainer.html(Math.ceil(response.currently.temperature) + "&deg;");
+    $('body').prepend(weatherContainer);
+    $('body').prepend(tempContainer);
+  },
+  error: function (error) {
+    console.log(error);
+  }
+});
